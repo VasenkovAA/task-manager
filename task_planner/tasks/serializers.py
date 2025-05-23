9add5e6089179785b7754d377ed9fcdc52346acf
VasenkovAA <andrej.500@mail.ru>
2025-05-23 17:26:04 +0300

    add views and serialazers

---
+++ task_planner/tasks/serializers.py
@@ -0,0 +1,126 @@
+from rest_framework import serializers
+
+from .models import (
+    TaskCategory,
+    NotificationMethod,
+    Location,
+    Link,
+    Task,
+    TaskLink,
+    FileAttachment
+)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class TaskCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskCategory
+        fields = '__all__'
+        read_only_fields = ['id']
+
+class NotificationMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationMethod
+        fields = '__all__'
+        read_only_fields = ['id']
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+        read_only_fields = ['id']
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at']
+
+class TaskLinkSerializer(serializers.ModelSerializer):
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
+    link = serializers.PrimaryKeyRelatedField(queryset=Link.objects.all())
+
+    class Meta:
+        model = TaskLink
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+class FileAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileAttachment
+        fields = '__all__'
+        read_only_fields = ['uploaded_at', 'task']
+
+class TaskSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    last_editor = UserSerializer(read_only=True)
+    assignee = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), 
+        allow_null=True
+    )
+    location = serializers.PrimaryKeyRelatedField(
+        queryset=Location.objects.all(), 
+        allow_null=True
+    )
+    
+    dependencies = serializers.PrimaryKeyRelatedField(
+        many=True, 
+        queryset=Task.objects.all()
+    )
+    categories = serializers.PrimaryKeyRelatedField(
+        many=True, 
+        queryset=TaskCategory.objects.all()
+    )
+    notifications = serializers.PrimaryKeyRelatedField(
+        many=True, 
+        queryset=NotificationMethod.objects.all()
+    )
+    links = TaskLinkSerializer(many=True, read_only=True)
+    attachments = FileAttachmentSerializer(many=True, read_only=True)
+    
+    status = serializers.ChoiceField(choices=Task.STATUS_CHOICES)
+    risk_level = serializers.ChoiceField(choices=Task.RISK_LEVEL_CHOICES)
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+        read_only_fields = [
+            'id', 'created_at', 'updated_at', 'deleted_at', 
+            'version', 'is_deleted', 'author', 'last_editor', 
+            'attachments', 'history'
+        ]
+
+    def create(self, validated_data):
+        dependencies = validated_data.pop('dependencies', [])
+        categories = validated_data.pop('categories', [])
+        notifications = validated_data.pop('notifications', [])
+        
+        task = Task.objects.create(**validated_data)
+        task.dependencies.set(dependencies)
+        task.categories.set(categories)
+        task.notifications.set(notifications)
+        return task
+
+    def update(self, instance, validated_data):
+        dependencies = validated_data.pop('dependencies', None)
+        categories = validated_data.pop('categories', None)
+        notifications = validated_data.pop('notifications', None)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        if dependencies is not None:
+            instance.dependencies.set(dependencies)
+        if categories is not None:
+            instance.categories.set(categories)
+        if notifications is not None:
+            instance.notifications.set(notifications)
+            
+        instance.save()
+        return instance
--- task_planner/tasks/views.py
+++ task_planner/tasks/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+# views.py
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import (
+    TaskCategory,
+    NotificationMethod,
+    Location,
+    Link,
+    Task,
+    TaskLink,
+    FileAttachment
+)
+from .serializers import (
+    TaskCategorySerializer,
+    NotificationMethodSerializer,
+    LocationSerializer,
+    LinkSerializer,
+    TaskSerializer,
+    TaskLinkSerializer,
+    FileAttachmentSerializer
+)
 
-# Create your views here.
+class TaskCategoryViewSet(viewsets.ModelViewSet):
+    queryset = TaskCategory.objects.all()
+    serializer_class = TaskCategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class NotificationMethodViewSet(viewsets.ModelViewSet):
+    queryset = NotificationMethod.objects.all()
+    serializer_class = NotificationMethodSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LinkViewSet(viewsets.ModelViewSet):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class TaskLinkViewSet(viewsets.ModelViewSet):
+    queryset = TaskLink.objects.all()
+    serializer_class = TaskLinkSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class FileAttachmentViewSet(viewsets.ModelViewSet):
+    queryset = FileAttachment.objects.all()
+    serializer_class = FileAttachmentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.filter(is_deleted=False)
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=True, methods=['post'])
+    def add_dependency(self, request, pk=None):
+        task = self.get_object()
+        dependency = Task.objects.get(pk=request.data['dependency_id'])
+        task.dependencies.add(dependency)
+        return Response({'status': 'dependency added'})
+
+    @action(detail=True, methods=['post'])
+    def remove_dependency(self, request, pk=None):
+        task = self.get_object()
+        dependency = Task.objects.get(pk=request.data['dependency_id'])
+        task.dependencies.remove(dependency)
+        return Response({'status': 'dependency removed'})
